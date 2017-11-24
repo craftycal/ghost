fb14ebff377f378ec2cb4aed886d79d3f85852f8
Callum Powell <callum@exceldp.co.nz>
2017-11-24 14:28:06 +1300

    small fixes

--- js/sideBar.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-  // $( "#click" ).click(function() {
-  //   $( "#pop" ).toggle();
-  // });
-  $( ".sideNavButton" ).click(function() {
-    $('.sideMenu').toggleClass('widthOff');
-    $('.sideNavButton').toggleClass('positionOff');
-  });
-});
